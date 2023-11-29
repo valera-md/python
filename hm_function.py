20f882fa94033b4b3639fa459d9ef1d1ad3529d6
valera-md <150794469+valera-md@users.noreply.github.com>
2023-11-29 05:45:10 +0200

    Add files via upload

---
+++ hm_function.py
@@ -0,0 +1,61 @@
+# hm_function.py
+# https://github.com/dorinesinenco/EDUQATION/blob/master/programming/python/functional/wrappers.ru.md
+def wrap_brackets(text):
+ return "(" + text + ")"
+# print(wrap_brackets("12345"))
+def wrap_square_brackets(text):
+ return "[" + text + "]"
+def wrap_angle_brackets(text):
+ return "<" + text + ">"
+s = "12345"
+s = wrap_brackets(s)
+s = wrap_square_brackets(s)
+s = wrap_square_brackets(s)
+s = wrap_square_brackets(s)
+s = wrap_angle_brackets(s)
+s = wrap_angle_brackets(s)
+s = wrap_angle_brackets(s)
+
+# https://github.com/dorinesinenco/EDUQATION/blob/master/programming/python/functional/input.helpers.ru.md
+def inputInt(message):
+ return int(input(message))
+n = inputInt("Enter the first integer: ")
+m = inputInt("Enter the second integer: ")
+print(n + m)
+def inputFloat(message):
+ return float(input(message))
+n = inputFloat("Enter the first float number: ")
+m = inputFloat("Enter the second float number: ")
+print(n + m)
+def inputBoolean(message):
+ return bool(input(message))
+n = inputBoolean("Enter a value to convert it to boolean type and check the truth or falsity of this value: ")
+print(n)
+
+# https://github.com/dorinesinenco/EDUQATION/blob/master/programming/python/functional/draw.lines.ru.md
+def drawLine(length, direction):
+ if(direction == 'h'):
+  print("-" * length)
+ elif(direction == 'v'):
+  print("|\n" * length)
+drawLine(5, 'h')
+drawLine(3, 'v')
+
+# HW1: upgrade the "hi()” function
+# + parameter <name> - name of a person
+# when call hi("en", "John") -> Hello John!
+def hi(lang, name): # "eng", "ru", "ro", ...
+ if lang == "eng":
+  print("Hello", name + "!")
+ elif lang == "ru":
+  print("Привет", name + "!")
+ elif lang == "ro":
+  print("Salut", name + "!")
+ else:
+  print(f"error {lang} is not recognized")
+# user, consumer
+# finction call
+hi("eng", "John") # +
+hi("ru", "Иван") # +
+hi("ro", "Ion")
+hi("xz", "")
